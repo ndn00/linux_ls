69cc2db7a33107a5a931d39349b8b9a88a3631ee
ndn00 <ndnguyen@sfu.ca>
2020-08-09 21:49:08 -0700

    WIP fixing formatting

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #define _GNU_SOURCE
 #define _DEFAULT_SOURCE
 #include <dirent.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <grp.h>
-#include <pwd.h>
 #include <time.h>
 
 #define SIZE_OF_DATE_AND_TIME 16
@@ -22,12 +21,14 @@ bool i_flag = false;
 bool l_flag = false;
 bool R_flag = false;
 int f_idx = 1;
+bool first = true;
+int level = 0;
 
 void getAndPrintGroup(const gid_t grpNum) {
   struct group *grp = getgrgid(grpNum);
 
   if (grp) {
-    printf("%s ", grp->gr_name);
+    printf("%s\t", grp->gr_name);
   } else {
     printf("No group name for %u found\n", grpNum);
   }
@@ -37,10 +38,10 @@ void getAndPrintUserName(const uid_t uid) {
   struct passwd *pw = getpwuid(uid);
 
   if (pw) {
-      printf("%s ", pw->pw_name);
+    printf("%s\t", pw->pw_name);
   } else {
-      perror("Hmm not found???");
-      printf("No name found for %u\n", uid);
+    perror("Hmm not found???");
+    printf("No name found for %u\n", uid);
   }
 }
 
@@ -48,32 +49,34 @@ void getAndPrintFileMode(const mode_t mode) {
   // Regular mode (1 bit)
   if (S_ISREG(mode)) printf("-");
   // Directory mode (1 bit)
-  else if (S_ISDIR(mode)) printf("d");
+  else if (S_ISDIR(mode))
+    printf("d");
   // Symbolic link mode (1 bit)
-  else if (S_ISLNK(mode)) printf("l");
-  
+  else if (S_ISLNK(mode))
+    printf("l");
+
   // Owner permission (3 bits)
-  printf(mode & S_IRUSR ? "r" : "-"); // Read
-  printf(mode & S_IWUSR ? "w" : "-"); // Write
-  printf(mode & S_IXUSR ? "x" : "-"); // Execute
+  printf(mode & S_IRUSR ? "r" : "-");  // Read
+  printf(mode & S_IWUSR ? "w" : "-");  // Write
+  printf(mode & S_IXUSR ? "x" : "-");  // Execute
 
   // Group permission (3 bits)
-  printf(mode & S_IRGRP ? "r" : "-"); // Read
-  printf(mode & S_IWGRP ? "w" : "-"); // Write
-  printf(mode & S_IXGRP ? "x" : "-"); // Execute
+  printf(mode & S_IRGRP ? "r" : "-");  // Read
+  printf(mode & S_IWGRP ? "w" : "-");  // Write
+  printf(mode & S_IXGRP ? "x" : "-");  // Execute
 
   // Other permission (3 bits)
-  printf(mode & S_IROTH ? "r" : "-"); // Read
-  printf(mode & S_IWOTH ? "w" : "-"); // Write
-  printf(mode & S_IXOTH ? "x" : "-"); // Execute
+  printf(mode & S_IROTH ? "r" : "-");  // Read
+  printf(mode & S_IWOTH ? "w" : "-");  // Write
+  printf(mode & S_IXOTH ? "x" : "-");  // Execute
 
-  printf(" ");
+  printf("\t");
 }
 
 void getAndPrintTime(time_t time) {
   char dateAndTime[SIZE_OF_DATE_AND_TIME];
   strftime(dateAndTime, sizeof(dateAndTime), "%b %e %H:%M", localtime(&time));
-  printf("%s ", dateAndTime);
+  printf("%s\t", dateAndTime);
 }
 
 void printEntry(struct stat stats, char *fileName) {
@@ -84,16 +87,15 @@ void printEntry(struct stat stats, char *fileName) {
   gid_t groupNum = stats.st_gid;
   off_t size = stats.st_size;
   time_t time = stats.st_mtime;
-//  if (i_flag) 
-    printf("%ld ", index);
-//  if (l_flag) {
+  if (i_flag) printf("%ld\t", index);
+  if (l_flag) {
     getAndPrintFileMode(mode);
-    printf("%ld ", hardLinksNum);
+    printf("%ld\t", hardLinksNum);
     getAndPrintUserName(uid);
     getAndPrintGroup(groupNum);
-    printf("%ld ", size);
+    printf("%ld\t", size);
     getAndPrintTime(time);
-//  }
+  }
   printf("%s\n", fileName);
 }
 
@@ -135,7 +137,7 @@ void get_dirname(char *pathname, char *dirname) {
     // TODO
   } else {
     ptr++;
-    strncpy(dirname, ptr, strlen(pathname) - (int)(ptr - pathname));
+    strncpy(dirname, ptr, strlen(pathname) + 1 + (int)(ptr - pathname));
   }
 }
 void get_path(char *pathname, char *dirname, char *path_buf) {
@@ -145,7 +147,7 @@ void get_path(char *pathname, char *dirname, char *path_buf) {
 }
 
 // pathname must not have "/" at the end
-void printDir(char *pathname, char *dirname) {
+void printDir(char *pathname) {
   // char dname_buf[1000];
   char path_buf[1000];
   stat_t buf;
@@ -155,25 +157,31 @@ void printDir(char *pathname, char *dirname) {
   entries = scandir(pathname, &nameList, NULL, alphasort);
 
   // get_dirname(pathname, dname_buf); // current dir name
-  printf("%s:\n", dirname);
+  if (level ? true : R_flag ? true : false) {
+    if (!level) {
+      printf("\n");
+    } else {
+      first = true;
+    }
+    printf("%s:\n", pathname);
+  }
+  level++;
 
   for (int idx = 0; idx < entries; ++idx) {
     if (strcmp(nameList[idx]->d_name, ".") != 0 &&
-        strcmp(nameList[idx]->d_name, "..") != 0 &&
-        strcmp(nameList[idx]->d_name, dirname) != 0) {
+        strcmp(nameList[idx]->d_name, "..") != 0) {
       get_path(pathname, nameList[idx]->d_name, path_buf);
-      get_stat(path_buf, &buf);
+      get_lstat(path_buf, &buf);
       printEntry(buf, nameList[idx]->d_name);
     }
   }
   for (int idx = 0; idx < entries; ++idx) {
     if (strcmp(nameList[idx]->d_name, ".") != 0 &&
-        strcmp(nameList[idx]->d_name, "..") != 0 &&
-        strcmp(nameList[idx]->d_name, dirname) != 0) {
+        strcmp(nameList[idx]->d_name, "..") != 0) {
       get_path(pathname, nameList[idx]->d_name, path_buf);
-      get_stat(path_buf, &buf);
+      get_lstat(path_buf, &buf);
       if (R_flag && S_ISDIR(buf.st_mode)) {
-        printDir(path_buf, nameList[idx]->d_name);
+        printDir(path_buf);
       }
     }
     free(nameList[idx]);
@@ -187,15 +195,16 @@ void handle_input(char *pathname) {
   get_dirname(pathname, dirname_buf);
   get_stat(pathname, &buf);
   if (S_ISDIR(buf.st_mode)) {
-    printDir(pathname, dirname_buf);
-  }
-  printEntry(buf, dirname_buf);
+    printDir(pathname);
+  } else
+    printEntry(buf, dirname_buf);
 }
 
 int main(int argc, char **argv) {
   handle_flags(argc, argv);
   for (; f_idx < argc; ++f_idx) {
     handle_input(argv[f_idx]);
+    level = 0;
   }
 
   return 0;
